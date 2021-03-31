ff99ec9b6678660c9add288a0060d00c8b8c4c5a
SL20032 <76879007+SL20032@users.noreply.github.com>
2021-03-31 21:04:45 +0200

    clk_ctrl_add

---
+++ av_db_clk_lib/clklib/clk_ctrl.h
@@ -0,0 +1,20 @@
+/*
+ * clk_ctrl.h
+ *
+ * Created: 31.03.2021 20:27:19
+ *  Author: stani
+ */ 
+
+
+
+
+#ifndef CLK_CTRL_H_
+#define CLK_CTRL_H_
+
+void config_clk(void);
+void change_clk_sorce_ext_clk(void);
+void change_clk_sorce_int_osc(void);
+
+
+
+#endif /* CLK_CTRL_H_ */
